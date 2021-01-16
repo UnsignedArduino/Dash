36666980888b540b4fdd70c0c8350fffb1ee36d4
Ckyiu <UnsignedArduino@outlook.com>
2021-01-16 18:21:42 -0500

    Add downwards facing spike

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -13,6 +13,10 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
 }
